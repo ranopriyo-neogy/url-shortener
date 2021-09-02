ec4b6144836e8e423c25d9f96bfd59d853e755f4
Ranopriyo Neogy <ranopriyoneogy@Tor-Ranopriyo-MBP13.local>
2021-09-02 16:29:08 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+// Connect to database
+connectDB();
+
+app.use(express.json({ extended: false}));
+
+// Define Routes
+app.use('/', require('./routes/index'));
+app.use('/api/url', require('./routes/url'));
+
+
+const PORT = 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
